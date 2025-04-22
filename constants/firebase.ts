7000990f224e838f748b30ce5ba4e50068048004
Animesh Kumbhakar <128497576+Animeshhit@users.noreply.github.com>
2025-04-22 16:33:12 +0530

    final commit

--- constants/firebase.ts
+++ constants/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAi5ceQaTpRpbsv4F-879k55RX5FfC9yo0",
-  authDomain: "electrostat-86370.firebaseapp.com",
-  projectId: "electrostat-86370",
-  storageBucket: "electrostat-86370.appspot.com",
-  messagingSenderId: "457158597401",
-  appId: "1:457158597401:web:ab456e03f8ba18ab3f0388",
-  databaseURL:
-    "https://electrostat-86370-default-rtdb.asia-southeast1.firebasedatabase.app",
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: PROCESS.ENV.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  databaseURL:process.env.DATABASEURL
 };
 
 const app = initializeApp(firebaseConfig);
